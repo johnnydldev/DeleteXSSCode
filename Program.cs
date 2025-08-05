6be6735900c793e70e41042e1090eb3fdb6d0374
johnnydldev <johnnydldev@gmail.com>
2025-08-04 22:37:27 -0600

    Upload of verifier XSS and test class.

---
+++ Program.cs
@@ -0,0 +1,181 @@
+﻿using System.Data.SqlTypes;
+using System.Data.SqlClient;
+
+namespace ThreadTest
+{
+    internal class Program
+    {
+
+        private static SessionInfo _CreateSession = new SessionInfo()
+        {
+            SessionId = Guid.NewGuid().ToString(),
+            Browser = "Firefox",
+            IPAddress = "192.168.20.45"
+        };
+
+        private static SessionInfo _SessionUpdated = new SessionInfo()
+        {
+            SessionId = Guid.NewGuid().ToString(),
+            Browser = "Edge",
+            IPAddress = "10.34.65.17"
+        };
+
+        private static SqlCommand _cmd;
+
+        private static SqlConnection _connection;
+
+        private static SqlDataReader _reader;
+
+        static void Main(string[] args)
+        {
+            //CreateSession();
+            //Thread _init = new Thread(()=> poolingSimulate());
+            //_init.Start();
+
+            try
+            {
+                string value = """
+                    Hola mi nombre es
+
+                    <div id='item-container'>
+                        <section class="item-user-name">
+                    
+                        </section>
+                    </div>
+                    
+                    <script type="module" src="../algo/path">
+                        document.getElementById('');
+                    </script>
+
+                    Juan
+
+                    
+                    """;
+
+                value = VerifierXSS.DeleteXSSText(value);
+
+                Console.WriteLine(value);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error {ex.Message}");
+
+            }
+
+        }//End main method
+
+        public static void CreateSession()
+        {
+            try
+            {
+                using (_connection = new SqlConnection(StringConnection.StringConnectionDB))
+                {
+                    _cmd = new("sp_CreateSession", _connection)
+                    {
+                        CommandType = System.Data.CommandType.StoredProcedure
+                    };
+                    _cmd.Parameters.AddWithValue("SessionId",_CreateSession.SessionId);
+                    _cmd.Parameters.AddWithValue("Browser", _CreateSession.Browser);
+                    _cmd.Parameters.AddWithValue("IPAddress", _CreateSession.IPAddress);
+
+                    _connection.Open();
+                    int IsCreated = _cmd.ExecuteNonQuery();
+
+                    string messageConsole = (IsCreated > 0) ? "Session info created successfully" : "Session info create failed";
+
+                    Console.WriteLine(messageConsole);
+                   
+                }//End using connection obj
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Has been occured the next error: {ex.Message}");
+            }//End catch
+        }//
+
+        public static void UpdateSession()
+        {
+            try
+            {
+                using (_connection = new SqlConnection(StringConnection.StringConnectionDB))
+                {
+                    _cmd = new("sp_UpdateSession", _connection)
+                    {
+                        CommandType = System.Data.CommandType.StoredProcedure
+                    };
+                    _cmd.Parameters.AddWithValue("SessionId", _CreateSession.SessionId);
+                    _cmd.Parameters.AddWithValue("Browser", _CreateSession.Browser);
+                    _cmd.Parameters.AddWithValue("IPAddress", _CreateSession.IPAddress);
+
+                    _connection.Open();
+                    int IsCreated = _cmd.ExecuteNonQuery();
+
+                    string messageConsole = (IsCreated > 0) ? "Session info created successfully" : "Session info create failed";
+
+                    Console.WriteLine(messageConsole);
+
+                }//End using connection obj
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Has been occured the next error: {ex.Message}");
+            }//End catch
+        }//
+
+        private static void poolingSimulate()
+        {
+            while (true)
+            {
+                GetSession();
+                Thread.Sleep(5000);
+            }//
+        }//
+
+
+        public static void GetSession()
+        {
+            SessionInfo sessionInfo = new SessionInfo();
+
+            try
+            {
+                using (_connection = new SqlConnection(StringConnection.StringConnectionDB))
+                {
+                    _cmd = new("sp_Get_Session_By_Id", _connection)
+                    {
+                        CommandType = System.Data.CommandType.StoredProcedure
+                    };
+
+                    _connection.Open();
+
+                    using (_reader = _cmd.ExecuteReader())
+                    {
+                        while (_reader.Read())
+                        {
+                            sessionInfo = new SessionInfo()
+                            {
+                                SessionId = _reader["SessionId"].ToString(),
+                                Browser = _reader["Browser"].ToString(),
+                                IPAddress = _reader["IPAddress"].ToString()
+                            };
+                        }//
+                    }//End reader use
+
+                    if (!string.IsNullOrEmpty(sessionInfo.SessionId))
+                    {
+                        Console.WriteLine($"The session id: {sessionInfo.SessionId}, Browser: {sessionInfo.Browser}, Ip address: {sessionInfo.IPAddress}");
+                    }
+
+                }//End using connection obj
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Has been occured the next error: {ex.Message}");
+            }//End catch
+
+        }//
+
+
+         
+
+    }//End Program class
+}//End namespace
---
+++ VerifierXSS.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ThreadTest
+{
+    public class VerifierXSS
+    {
+
+        public static string DeleteXSSText(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return value;
+
+            string valueSanitized = value, pattern = @"(?<xss>((<{1}.*>{1})(.)(<{1}/{1}([a-z|A-Z])>{1})*))";
+
+            try
+            {
+                Regex rgx = new Regex(pattern, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+                Match match = rgx.Match(valueSanitized);
+
+                if (match.Success)
+                {
+                   string matchXss = match.Groups["xss"].Value;
+
+                    if (!string.IsNullOrEmpty(matchXss))
+                    {
+                        valueSanitized = valueSanitized.Replace(matchXss, string.Empty);
+
+                        DeleteXSSText(valueSanitized);
+                    }
+
+                }//End verify
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine($"Error on tried delete xss data, info: {ex.Message}");
+            }
+
+            return valueSanitized;
+
+        }//End delete XSS text method
+
+
+    }//End verifier XSS text
+}//End namespace
